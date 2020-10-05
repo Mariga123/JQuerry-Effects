8f747a64ec89a977333f9461879d11cc66d0bcb4
Mariga123 <johnmariga8@gmail.com>
2020-10-05 15:35:52 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// $(document).ready(function () {
+//     $("p").click(function () {
+//         $(".walrus-showing").toggle();
+//         $(".walrus-hidden").toggle();
+//     });
+// });
+// $(document).ready(function () {
+//     $(".clickable").click(function () {
+//         $(".walrus-showing").toggle();
+//         $(".walrus-hidden").toggle();
+//     });
+// });
+$(document).ready(function () {
+    $(".clickable").click(function () {
+        $("#walrus-showing").toggle();
+        $("#walrus-hidden").toggle();
+    });
+});
